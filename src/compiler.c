206a3c212d49fd26a3a785083d0bd6cc5c38ce66
z04a <lodbipb6@gmail.com>
2024-10-24 19:28:55 +0300

    feat: implemented backpatching

--- src/compiler.c
+++ src/compiler.c
@@ -15,6 +15,24 @@
 #include "util/comp.h"
 #include "arch.h"
 
+struct compile_label {
+	u16 start_ptr;
+	char *label;
+};
+
+struct compile_fix {
+	u16 fix_ptr;
+	char *label;
+};
+
+struct compile_bytecode {
+	u16 *bytecode;
+	struct compile_label *labels;
+	u16 labels_count;
+	struct compile_fix *fixes;
+	u16 fixes_count;
+};
+
 /* I've implemented check for byte array size, but please -> do not change it!*/
 /* But it also may change it the future, so keeping it for now */
 
@@ -65,8 +83,6 @@ void destroy_byte_array(u16 *array) {
 	free(array);
 }
 
-
-
 bool create_binary(u16 *array) {
 	char *filename = "output.bin";
 	FILE *fp;
@@ -107,10 +123,38 @@ void read_binary(char *file) {
 }
 
 
-void usage_panic(char *argv[]) {
-	printf("err: provide path to ins.asm\n");
-	printf("usage: %s <path/to/ins.asm>\n", argv[0]);
-	exit(1);
+
+void append_label(struct compile_bytecode *compiler, code_block block, u16 start_pointer) {
+	if (compiler->labels_count == 0) {
+		compiler->labels_count++;
+		compiler->labels = malloc(compiler->labels_count * sizeof(struct compile_label));
+		compiler->labels[compiler->labels_count - 1].start_ptr = start_pointer;
+		compiler->labels[compiler->labels_count - 1].label = block.label;
+	} else {
+		compiler->labels_count++;
+		compiler->labels = realloc(compiler->labels, compiler->labels_count * sizeof(struct compile_label));
+		compiler->labels[compiler->labels_count - 1].start_ptr = start_pointer;
+		compiler->labels[compiler->labels_count - 1].label = block.label;
+	}
+}
+
+void append_fix(struct compile_bytecode *compiler, u16 fix_ptr, char *label) {
+	// TODO
+	// This is for debug only!
+	compiler->bytecode[fix_ptr] = 0xFFFF;
+	//
+
+	if (compiler->fixes_count == 0) {
+		compiler->fixes_count++;
+		compiler->fixes = malloc(compiler->fixes_count * sizeof(struct compile_label));
+		compiler->fixes[compiler->fixes_count - 1].fix_ptr = fix_ptr;
+		compiler->fixes[compiler->fixes_count - 1].label = label;
+	} else {
+		compiler->fixes_count++;
+		compiler->fixes = realloc(compiler->fixes, compiler->fixes_count * sizeof(struct compile_label));
+		compiler->fixes[compiler->fixes_count - 1].fix_ptr = fix_ptr;
+		compiler->fixes[compiler->fixes_count - 1].label = label;
+	}
 }
 
 void append_value(u16 *bytecell, u16 value, u16 offset, u16 length) {
@@ -120,58 +164,60 @@ void append_value(u16 *bytecell, u16 value, u16 offset, u16 length) {
 	*bytecell = *bytecell | ((value << offset) & value_mask); // AND with value_mask is for extra safety
 }
 
-void append_bytearray(u16 *bytecode, code_block block, u16 *start_pointer) {
+void append_bytearray(struct compile_bytecode *compiler, code_block block, u16 *start_pointer) {
 	// printf("APPENDING %s at %d\n", block.label, *start_pointer);
 	// TODO
 	// add start_pointer of each block to some array
 	// so it can be used in fixes for jumps
 	for (size_t i = 0; i < block.ins.count; ++i) {
 		// printf("Putting: %s\n", block.ins.cmds[i].ins.token);
-		bytecode[*start_pointer] = block.ins.cmds[i].ins.opcode; //appending opcode
+		compiler->bytecode[*start_pointer] = block.ins.cmds[i].ins.opcode; //appending opcode
 		
 		u16 hint_count = 0;
 
-		if (block.ins.cmds[i].val2_type == T_VAL2_NULL) {
-			printf("%s -> NULL TYPE\n", block.ins.cmds[i].ins.token);
-		}
+		// if (block.ins.cmds[i].val2_type == T_VAL2_NULL) {
+		// 	printf("%s -> NULL TYPE\n", block.ins.cmds[i].ins.token);
+		// }
 		// appending VAL1
 		if (block.ins.cmds[i].val1_type == T_VAL1_REG) {
-			append_value(&bytecode[*start_pointer], block.ins.cmds[i].val1.reg, 5, 3);
+			append_value(&compiler->bytecode[*start_pointer], block.ins.cmds[i].val1.reg, 5, 3);
 		} else {
 			// put HINT
 			if (block.ins.cmds[i].ins.opcode != HALT_OPCODE && block.ins.cmds[i].val1_type != T_VAL1_NULL ) {
-				append_value(&bytecode[*start_pointer], HINT_FLAG, 5, 3);
+				append_value(&compiler->bytecode[*start_pointer], HINT_FLAG, 5, 3);
 				hint_count++;
 				if (block.ins.cmds[i].val1_type == T_VAL1_U16) {
-					printf("Putting u16 %d\n", block.ins.cmds[i].val1.num);
-					bytecode[*start_pointer + hint_count] = block.ins.cmds[i].val1.num;
+					// printf("Putting u16 %d\n", block.ins.cmds[i].val1.num);
+					compiler->bytecode[*start_pointer + hint_count] = block.ins.cmds[i].val1.num;
 				}
 				else if (block.ins.cmds[i].val1_type == T_VAL1_LABEL)
-					bytecode[*start_pointer + hint_count] = 0xFFFF;
+					// compiler->bytecode[*start_pointer + hint_count] = 0xFFFF;
+					append_fix(compiler, *start_pointer + hint_count, block.ins.cmds[i].val1.label);
 				else if (block.ins.cmds[i].val1_type == T_VAL1_ADDRESS) {
 					printf("it shouldn't be ADDRESS, Please FIX!!!!!!\n");
-					bytecode[*start_pointer + hint_count] = 0xFFFF;
+					compiler->bytecode[*start_pointer + hint_count] = 0xFFFF;
 				}
 			}
 		}
 
 		// Appending VAL2
 		if (block.ins.cmds[i].val2_type == T_VAL2_REG) {
-			append_value(&bytecode[*start_pointer], block.ins.cmds[i].val2.reg, 8, 3);
+			append_value(&compiler->bytecode[*start_pointer], block.ins.cmds[i].val2.reg, 8, 3);
 		} else {
 			// put HINT
 			if (block.ins.cmds[i].ins.opcode != HALT_OPCODE && block.ins.cmds[i].val2_type != T_VAL2_NULL) {
-				append_value(&bytecode[*start_pointer], HINT_FLAG, 8, 3);
+				append_value(&compiler->bytecode[*start_pointer], HINT_FLAG, 8, 3);
 				hint_count++;
 				if (block.ins.cmds[i].val2_type == T_VAL2_U16) {
-					printf("Putting u16 %d\n", block.ins.cmds[i].val2.num);
-					bytecode[*start_pointer + hint_count] = block.ins.cmds[i].val2.num;
+					// printf("Putting u16 %d\n", block.ins.cmds[i].val2.num);
+					compiler->bytecode[*start_pointer + hint_count] = block.ins.cmds[i].val2.num;
 				}
 				else if (block.ins.cmds[i].val2_type == T_VAL2_LABEL)
-					bytecode[*start_pointer + hint_count] = 0xFFFF;
+					// compiler->bytecode[*start_pointer + hint_count] = 0xFFFF;
+					append_fix(compiler, *start_pointer + hint_count, block.ins.cmds[i].val2.label);
 				else if (block.ins.cmds[i].val2_type == T_VAL2_ADDRESS) {
 					printf("it shouldn't be ADDRESS, Please FIX!!!!!!\n");
-					bytecode[*start_pointer + hint_count] = 0xFFFF;
+					compiler->bytecode[*start_pointer + hint_count] = 0xFFFF;
 				}
 			}
 		}
@@ -181,21 +227,22 @@ void append_bytearray(u16 *bytecode, code_block block, u16 *start_pointer) {
 		// because it's always used in conditional jumps
 		// Appending VAL3
 		if (block.ins.cmds[i].val3_type == T_VAL3_REG) {
-			append_value(&bytecode[*start_pointer], block.ins.cmds[i].val3.reg, 11, 3);
+			append_value(&compiler->bytecode[*start_pointer], block.ins.cmds[i].val3.reg, 11, 3);
 		} else {
 			// put HINT
 			if (block.ins.cmds[i].ins.opcode != HALT_OPCODE && block.ins.cmds[i].val3_type != T_VAL3_NULL) {
-				append_value(&bytecode[*start_pointer], HINT_FLAG, 11, 3);
+				append_value(&compiler->bytecode[*start_pointer], HINT_FLAG, 11, 3);
 				hint_count++;
 				if (block.ins.cmds[i].val3_type == T_VAL3_U16) {
-					printf("Putting u16 %d\n", block.ins.cmds[i].val3.num);
-					bytecode[*start_pointer + hint_count] = block.ins.cmds[i].val3.num;
+					// printf("Putting u16 %d\n", block.ins.cmds[i].val3.num);
+					compiler->bytecode[*start_pointer + hint_count] = block.ins.cmds[i].val3.num;
 				}
 				else if (block.ins.cmds[i].val3_type == T_VAL3_LABEL)
-					bytecode[*start_pointer + hint_count] = 0xFFFF;
+					// compiler->bytecode[*start_pointer + hint_count] = 0xFFFF;
+					append_fix(compiler, *start_pointer + hint_count, block.ins.cmds[i].val3.label);
 				else if (block.ins.cmds[i].val3_type == T_VAL3_ADDRESS) {
 					printf("it shouldn't be ADDRESS, Please FIX!!!!!!\n");
-					bytecode[*start_pointer + hint_count] = 0xFFFF;
+					compiler->bytecode[*start_pointer + hint_count] = 0xFFFF;
 				}
 			}
 		}
@@ -204,22 +251,53 @@ void append_bytearray(u16 *bytecode, code_block block, u16 *start_pointer) {
 	}
 }
 
-void find_start(u16 *bytecode, code_blocks *blocks, u16 *start_pointer) {
+
+void find_start(struct compile_bytecode *compiler, code_blocks *blocks, u16 *start_pointer) {
 	for (size_t i = 0 ; i < blocks->count; ++i) {
 		if (strcmp(blocks->block[i].label, "start") == 0) {
-			append_bytearray(bytecode, blocks->block[i], start_pointer);
+			u16 start = *start_pointer;
+			append_bytearray(compiler, blocks->block[i], start_pointer);
+			append_label(compiler, blocks->block[i], start);
 		} else continue;
 	}
 }
 
+void fix_bytecode(struct compile_bytecode *compiler) {
+	for (int fix = 0; fix < compiler->fixes_count; ++fix) {
+		printf("Trying to fix HINT <%s>@%d...\n", compiler->fixes[fix].label, compiler->fixes[fix].fix_ptr);
+		char *fix_label = compiler->fixes[fix].label;
+
+		for (int label = 0; label < compiler->labels_count; ++label) {
+			if (strcmp(fix_label, compiler->labels[label].label) == 0) {
+				printf("Found fix, applying...\n");
+				compiler->bytecode[compiler->fixes[fix].fix_ptr] = compiler->labels[label].start_ptr;
+				break;
+			}
+			if (label == compiler->labels_count - 1) {
+				printf("[PANIC] Failed to found fix for this HINT\n");
+				printf("[PANIC] Exiting compiler, sorry.\n");
+				exit(1);
+			}
+		}
+	}
+}
+
+void usage_panic(char *argv[]) {
+	printf("err: provide path to ins.asm\n");
+	printf("usage: %s <path/to/ins.asm>\n", argv[0]);
+	exit(1);
+}
+
 int main(int argc, char **argv) {
+	// TODO
+	// add argument parser for compiler (and for interpreter too)
 	if (argc == 1) {
 		usage_panic(argv);
 	}
 	// TODO
 	// Check for valid file, don't segfault!
 	char *file = argv[1];
-	printf("ERROR: COMPILER IS NOT FINISHED. EXITING...\n");
+	// printf("ERROR: COMPILER IS NOT FINISHED. EXITING...\n");
 	// return(1);
 
 	if (READ_BINARY) {
@@ -227,7 +305,8 @@ int main(int argc, char **argv) {
 		return 0;
 	}
 
-	u16 *bytecode = create_byte_array();
+	struct compile_bytecode compiler = {0};
+	compiler.bytecode = create_byte_array();
 
 
 	instruction_set isa = init_isa();
@@ -240,16 +319,26 @@ int main(int argc, char **argv) {
 	printf("Code blocks: %d\n", code.count);
 
 	u16 bytecode_ptr = BYTECODE_START;
-	find_start(bytecode, &code, &bytecode_ptr);
+	find_start(&compiler, &code, &bytecode_ptr);
 
 	for (size_t i = 0 ; i < code.count; ++i) {
 		if (strcmp(code.block[i].label, "start") != 0) {
 			// printf("Code block %zu:\n", i);
 			// printf("Name: %s\n", code.block[i].label);
-			append_bytearray(bytecode, code.block[i], &bytecode_ptr);
-			printf("\n");
+			u16 start = bytecode_ptr;
+			append_bytearray(&compiler, code.block[i], &bytecode_ptr);
+			append_label(&compiler, code.block[i], start);
+			// printf("\n");
 			continue;
 		}
+	}
+
+	printf("\n--------------------\n");
+	printf("First pass completed.\n");
+	if (compiler.fixes_count > 0) {
+		printf("Need to fix %d instances.\n", compiler.fixes_count);
+	} else {
+		printf("No need to fix anything, skipping second pass.");
 	}
 
 	printf("start pointer %d\n", bytecode_ptr);
@@ -260,8 +349,23 @@ int main(int argc, char **argv) {
 
 	/* REMOVE to here */
 
-	print_byte_array(bytecode, 0, 62);
+	print_byte_array(compiler.bytecode, 0, bytecode_ptr + 1);
 
+	printf("---------------\n");
+	printf("Number of labels: %d\n", compiler.labels_count);
+	for (int i = 0; i < compiler.labels_count; ++i) {
+		printf("[%d] -> %s (starts at: %d)\n", i, compiler.labels[i].label, compiler.labels[i].start_ptr);
+	}
+	printf("\n");
+	printf("Number of fixes: %d\n", compiler.fixes_count);
+	for (int i = 0; i < compiler.fixes_count; ++i) {
+		printf("[%d] -> %s (starts at: %d)\n", i, compiler.fixes[i].label, compiler.fixes[i].fix_ptr);
+	}
+	printf("\n");
+
+	fix_bytecode(&compiler);
+
+	print_byte_array(compiler.bytecode, 0, bytecode_ptr + 1);
 	// printf("Creating binary...\n");
 	// create_binary(bytecode);
 	goto clean;
@@ -269,6 +373,12 @@ int main(int argc, char **argv) {
 clean:
 	// TODO (check if ISA was correctly initialized)
 	free(isa.ins);
-	destroy_byte_array(bytecode);
+
+	if (compiler.labels_count > 0)
+		free(compiler.labels);
+	if (compiler.fixes_count > 0)
+		free(compiler.fixes);
+
+	destroy_byte_array(compiler.bytecode);
 	return 0;
 }
